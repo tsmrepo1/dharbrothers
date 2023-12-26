0b9f320272615696025bcf2773aeb81e2e2ba87d
unknown <it@thinksurfmedia.com>
2023-12-26 12:23:34 +0530

    19 commit

--- php/resources/views/components/authentication-card-logo.blade.php
+++
@@ -1,5 +0,0 @@
-<a href="/">
-    <div style="padding: 10px; border-radius: 10px; background-color: white;">
-        <img src="./assets/images/aps_logo.png" style="height: 100px; width: 200px;" />
-    </div>
-</a>
--- php/resources/views/pages/thankyou.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.web')
-
-@section('content')
-<div class="card shadow p-5">
-    <div class="card-body">
-        <h2>Thank You</h2>
-    </div>
-</div>
-@stop
